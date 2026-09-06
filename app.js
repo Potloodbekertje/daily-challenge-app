e1dd485cec6cf0c2ae9f832091fca5c25379f031
Potloodbekertje <130544755+Potloodbekertje@users.noreply.github.com>
2026-09-06 17:00:33 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js');
+}
+
+const loginScreen = document.getElementById('login-screen');
+const appScreen = document.getElementById('app-screen');
+
+window.onload = () => {
+  const savedName = localStorage.getItem('dc_name');
+  const savedPin = localStorage.getItem('dc_pin');
+  
+  if (savedName && savedPin) {
+    showAppScreen(savedName, savedPin);
+  } else {
+    loginScreen.classList.remove('hidden');
+  }
+};
+
+function saveLogin() {
+  const name = document.getElementById('input-name').value;
+  const pin = document.getElementById('input-pin').value;
+  
+  if(!name || !pin) return;
+
+  localStorage.setItem('dc_name', name);
+  localStorage.setItem('dc_pin', pin);
+  
+  loginScreen.classList.add('hidden');
+  showAppScreen(name, pin);
+}
+
+function logout() {
+  localStorage.removeItem('dc_name');
+  localStorage.removeItem('dc_pin');
+  window.location.reload();
+}
+
+async function showAppScreen(name, pin) {
+  appScreen.classList.remove('hidden');
+  document.getElementById('welcome-text').innerText = "Hallo " + name;
+  
+  try {
+    const response = await fetch(APP_CONFIG.API_URL, {
+      method: 'POST',
+      body: JSON.stringify({ action: 'getToday', pin: pin })
+    });
+    
+    const data = await response.json();
+    document.getElementById('loading').classList.add('hidden');
+    document.getElementById('challenge-card').classList.remove('hidden');
+
+    if (data.error) {
+      logout();
+      return;
+    }
+
+    if (!data.challenge) {
+      document.getElementById('challenge-waiting').classList.remove('hidden');
+      document.getElementById('partner-name').innerText = "Niemand";
+      return;
+    }
+
+    if (data.challenge.gebruiker === name) {
+      document.getElementById('challenge-active').classList.remove('hidden');
+      document.getElementById('cat-badge').innerText = data.challenge.categorie;
+      document.getElementById('chal-text').innerText = data.challenge.challenge;
+    } else {
+      document.getElementById('challenge-waiting').classList.remove('hidden');
+      document.getElementById('partner-name').innerText = data.challenge.gebruiker;
+    }
+    
+  } catch (err) {
+    logout();
+  }
+}
---
+++ config.js
@@ -0,0 +1,4 @@
+const APP_CONFIG = {
+	// HIER_AANPASSEN: Plak hier jouw gekopieerde Web-app URL
+  API_URL: "https://script.google.com/macros/s/AKfycbyFVB4kcZRTk8_KE3R-BDOsfUJcrh_01bOSbnI0MIp429bf9DempuKlKmTUg85SPkSG/exec"
+};
