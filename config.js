c3ddf63d6eafa44a6ca02d4972fbf79ee64fd5f3
Potloodbekertje <130544755+Potloodbekertje@users.noreply.github.com>
2026-09-11 15:18:23 +0200

    config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const APP_CONFIG = {
-  // Vul hieronder jouw gekopieerde Web-app URL in (let op dat de aanhalingstekens en de komma blijven staan!)
+  // Vul hieronder jouw gekopieerde Web-app URL in
   API_URL: "https://script.google.com/macros/s/AKfycbyFVB4kcZRTk8_KE3R-BDOsfUJcrh_01bOSbnI0MIp429bf9DempuKlKmTUg85SPkSG/exec",
   
-  // Vul hier jullie namen in
+  // Maak dit object 'bevroren' zodat de app deze nooit per ongeluk kan overschrijven
   USERS: ["Onno", "Kelly"]
 };
+Object.freeze(APP_CONFIG);
